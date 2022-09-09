7af07199a4907fd8315acbf81bfce56c57d90e18
Tommy Gibson <tommy.a.gibson@gmail.com>
2022-09-08 17:12:06 -0700

    Narrowed scope to RFs with few studies, potential small sd(RE)
    There are 6 risk factors with > 2 studies and at least one random effect with sample SD < 0.25 for

--- syncope-example.R
+++ syncope-example.R
@@ -7,7 +7,7 @@ library(loo)
 
 source(here("CTS-functions.R"))
 
-
+options(mc.cores = parallel::detectCores())
 # define the models
 
 re3.std.model <- stan_model(here("Models", "3RE.stan"))
@@ -19,13 +19,31 @@ re3.reglambda.model <- stan_model(here("Models", "3RE-regularized-lambda-cholesk
 # load syncope data, filter out vars with only 2-4 observations
 # also remove studies without counts
 
-syncope <- read_csv(here("syncope.cleaned.csv")) %>%
+syncope <- read_csv(here("Data", "syncope.cleaned.csv")) %>%
   group_by(Variable) %>%
   filter(!is.na(n_i0),
-         n() > 4)
+         n() > 2)
+
+syncope_re_hats <- syncope %>%
+  transmute(Variable = Variable,
+            nu.hat = log((n_i1 / N_i) / (1 - n_i1 / N_i)),
+            beta.hat = 1 / 2 * (log((y_i1 / n_i1) / (1 - y_i1 / n_i1)) + log((y_i0 / n_i0) / (1 - y_i0 / n_i0))),
+            delta.hat = lnORhat) %>%
+  group_by(Variable) %>%
+  summarize(sd.nu.hat = sd(nu.hat),
+            sd.sd.nu.hat = sd.nu.hat * sqrt(1 - 2 / (n() - 1) * (gamma(n() / 2) / gamma((n() - 1) / 2))^2),
+            sd.beta.hat = sd(beta.hat),
+            sd.sd.beta.hat = sd.beta.hat * sqrt(1 - 2 / (n() - 1) * (gamma(n() / 2) / gamma((n() - 1) / 2))^2),
+            sd.delta.hat = sd(delta.hat),
+            sd.sd.delta.hat = sd.beta.hat * sqrt(1 - 2 / (n() - 1) * (gamma(n() / 2) / gamma((n() - 1) / 2))^2)) %>%
+  filter(sd.nu.hat < 0.25 | sd.beta.hat < 0.25 | sd.delta.hat < 0.25)
+
+syncope_small_sds <- syncope %>%
+  filter(Variable %in% syncope_re_hats$Variable)
+            
 
 # start with male gender as an example
-male <- syncope %>%
+male <- syncope_small_sds %>%
   ungroup %>%
   filter(Variable == "Male Gender")
 
@@ -67,43 +85,75 @@ re3.dat.reghorse <- list(S = S, y1 = y[,1], y0 = y[,2], n1 = n[,1], n0 = n[,2],
 re3.dat.reglambda <- list(S = S, y1 = y[,1], y0 = y[,2], n1 = n[,1], n0 = n[,2],
                          a = 0, b = 2, c = 0, d = 2, f = 0, g = 2,
                          scale_global_lambda = scale_global_lambda, scale_gamma = 8,
-                         nu_global_lambda = 1,  nu_local_lambda = 1,
-                         slab_scale_lambda = 2, slab_df_lambda = 4)
+                         nu_global_lambda = 3,  nu_local_lambda = 1,
+                         slab_scale_lambda = 1, slab_df_lambda = 1)
 # scale_global_lambda = 1, scale_global_gamma = 1,
 # nu_global_lambda = 1, nu_global_gamma = 1,
 # nu_local_lambda = 1, nu_local_gamma = 1,
 # slab_scale_lambda = 2, slab_df_lambda = 4,
 # slab_scale_gamma = 1, slab_df_gamma = 8
-re3.stan <- sampling(re3.model, data = re3.dat.stan,
+re3.stan <- sampling(re3.std.model, data = re3.dat.stan,
                      chains = 4, iter = 4000,
                      control = list(adapt_delta = 0.99, max_treedepth = 15))
 re3.iw.stan <- sampling(re3.iw.model, data = re3.dat.stan,
                         chains = 4, iter = 4000,
-                        pars = c("theta0", "SD", "CORR"))#,
+                        pars = c("theta0", "SD", "CORR", "Sigma"))#,
                         #control = list(adapt_delta = 0.95))
 
 re3.lkj.stan <- sampling(re3.lkj.model, data = re3.dat.stan,
                          chains = 4, iter = 4000, # iter = 10000, warmup = 6000,
-                         pars = c("theta0", "Sigma"),
+                         pars = c("theta0", "SD", "CORR", "Sigma"),
                          control = list(adapt_delta = .99, max_treedepth = 15),
                          cores = 4)
-
-re3.reghorse <- sampling(re3.reghorsechol.model, data = re3.dat.reghorse,
-                             chains = 4, iter = 5000, warmup = 2500,
-                             pars = c("theta0", "SD", "CORR"),
-                             control = list(adapt_delta = .99, max_treedepth = 20),
-                             cores = 4)
+# 
+# re3.reghorse <- sampling(re3.reghorse.model, data = re3.dat.reghorse,
+#                              chains = 4, iter = 4000, warmup = 2000,
+#                              pars = c("theta0", "SD", "CORR"),
+#                              control = list(adapt_delta = .99, max_treedepth = 15),
+#                              cores = 4)
 re3.reglambda <- sampling(re3.reglambda.model, data = re3.dat.reglambda,
-                          chains = 4, iter = 12000, warmup = 6000,
-                          pars = c("theta0", "Sigma"),
+                          chains = 4, iter = 4000, warmup = 2000,
+                          pars = c("theta0", "SD", "CORR", "Sigma"),
                           control = list(adapt_delta = 0.99, max_treedepth = 15),
                           cores = 4)
 
 round(summary(re3.stan, pars = c("beta0", "delta0", "nu0", "sigma_beta", "sigma_delta", "sigma_nu", "lp__"))$summary, 4)
-round(summary(re3.iw.stan)$summary, 4)
-round(summary(re3.lkj.stan)$summary, 4)
-round(summary(re3.reghorse)$summary, 4)
-round(summary(re3.reglambda)$summary, 4)
+round(summary(re3.iw.stan, pars = c("theta0", "SD", "CORR"))$summary, 4)
+round(summary(re3.lkj.stan, pars = c("theta0", "SD", "CORR"))$summary, 4)
+# round(summary(re3.reghorse)$summary, 4)
+round(summary(re3.reglambda, pars = c("theta0", "SD", "CORR"))$summary, 4)
+
+# plot sds and correlations
+par(mfrow = c(3, 1))
+hist(rstan::extract(re3.iw.stan, pars = "SD")$SD[,1], xlim = c(0, 2), breaks = 30)
+hist(rstan::extract(re3.lkj.stan, pars = "SD")$SD[,1], xlim = c(0, 2), breaks = 30)
+hist(rstan::extract(re3.reglambda, pars = "SD")$SD[,1], xlim = c(0, 2), breaks = 30)
+hist(rstan::extract(re3.iw.stan, pars = "SD")$SD[,2], xlim = c(0, 0.5), breaks = 30)
+hist(rstan::extract(re3.lkj.stan, pars = "SD")$SD[,2], xlim = c(0, 0.5), breaks = 30)
+hist(rstan::extract(re3.reglambda, pars = "SD")$SD[,2], xlim = c(0, 0.5), breaks = 30)
+hist(rstan::extract(re3.iw.stan, pars = "SD")$SD[,3], xlim = c(0, 1), breaks = 30)
+hist(rstan::extract(re3.lkj.stan, pars = "SD")$SD[,3], xlim = c(0, 0.25), breaks = 30)
+hist(rstan::extract(re3.reglambda, pars = "SD")$SD[,3], xlim = c(0, 0.25), breaks = 30)
+
+par(mfrow = c(3, 3))
+for(i in 1:3){
+  for(j in 1:3){
+    hist(rstan::extract(re3.iw.stan, pars = "CORR")$CORR[,i,j], xlim = c(-1, 1), breaks = 30)
+  }
+}
+for(i in 1:3){
+  for(j in 1:3){
+    hist(rstan::extract(re3.lkj.stan, pars = "CORR")$CORR[,i,j], xlim = c(-1, 1), breaks = 30)
+  }
+}
+for(i in 1:3){
+  for(j in 1:3){
+    hist(rstan::extract(re3.reglambda, pars = "CORR")$CORR[,i,j], xlim = c(-1, 1), breaks = 30)
+  }
+}
+for(i in 1:3){
+  hist(rstan::extract(re3.reglambda, pars = "SD")$SD[,i], breaks = 20)
+}
 
 # reglambda.sims <- rstan::extract(re3.reglambda, pars = c("theta0", "Sigma"))
 make_mean_var_sims <- function(sims.list){
@@ -118,13 +168,19 @@ make_mean_var_sims <- function(sims.list){
 
 reglambda.sims <- make_mean_var_sims(rstan::extract(re3.reglambda, pars = c("theta0", "Sigma")))
 lkj.sims <- make_mean_var_sims(rstan::extract(re3.lkj.stan, pars = c("theta0", "Sigma")))
+iw.sims <- make_mean_var_sims(rstan::extract(re3.iw.stan, pars = c("theta0", "Sigma")))
 cts0_from_one_posterior(reglambda.sims[1,])
-reglambda.cts0.summary <- as.data.frame(cts_summary_from_gamma(reglambda.sims, M = 1000)) %>%
+reglambda.cts0.summary <- as.data.frame(cts_summary_from_gamma(reglambda.sims, M = 500)) %>%
   type_convert() %>%
   rename(CTS =  V1,
          Mean = V2,
          SD =   V3)
-lkj.cts0.summary <- as.data.frame(cts_summary_from_gamma(lkj.sims, M = 1000)) %>%
+lkj.cts0.summary <- as.data.frame(cts_summary_from_gamma(lkj.sims, M = 500)) %>%
+  type_convert() %>%
+  rename(CTS = V1,
+         Mean = V2,
+         SD = V3)
+iw.cts0.summary <- as.data.frame(cts_summary_from_gamma(iw.sims, M = 500)) %>%
   type_convert() %>%
   rename(CTS = V1,
          Mean = V2,
