fd160ff179237045a7be7504b2544f99fce6f57d
Tommy Gibson <tommy.a.gibson@gmail.com>
2022-10-10 12:33:03 -0700

    Functions to calculate AR
    In Monte Carlo style like in Paper 1. E[p_it | mu_t, delta_t]

---
+++ AR-functions.R
@@ -0,0 +1,37 @@
+##### Functions for calculating ARs, LORs, etc from mean/variance
+##### similar to calculating CTS_0 from mean/variance estimates in 3RE model
+
+# function to calculate AR for each of T treatments from one posterior sample
+# data structure (mean.sd) is (mu1, mu2,..., muT, sd1, ..., sdT)
+
+
+get_AR <- function(mean.sd, M = 1000){
+  ntrt <- length(mean.sd) / 2
+  mu <- mean.sd[1:ntrt]
+  sigma <- mean.sd[(ntrt + 1):(2 * ntrt)]
+  
+  pred <- mapply(function(x, y){1 / (1 + exp(-rnorm(M, mean = x, sd = y)))},
+                 mu, sigma)
+  
+  return(apply(pred, 2, mean))
+}
+
+get_full_AR <- function(mean.sd.mat, M = 1000){
+  t(apply(X = mean.sd.mat, MARGIN = 1, FUN = get_AR, M = M))
+}
+
+simple_summary <- function(x){
+  return(c(mean(x), sd(x), quantile(x, c(.025, .975))))
+}
+summarize_full_AR <- function(mean.sd.mat, M = 1000){
+  
+  full_AR <- get_full_AR(mean.sd.mat, M = M)
+  
+  if(is.null(dim(full_AR))){
+    return(simple_summary(full_AR))
+  } 
+  
+  else {
+    return(t(apply(full_AR, 2, simple_summary)))
+  }
+}
